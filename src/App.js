3c556fe6487ec1e76c7700fe37c86ce8feb601ae
TheSoundOfGoodBye <karbehemoth@gmail.com>
2024-11-20 23:44:47 +0900

    add relic container and name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import all images from ./images folder
+function importAllImage(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAllImage(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contents">
+        <div className="top-section">
+
+        </div>
+        <div className="left-section">
+          <h4 className="head-text">Synergy</h4>
+          <img src={images['synergy_layout.jpg']} className="synergy" alt="synergy"/>
+        </div>
+        <div className="right-section">
+          <h4 className="head-text">Artifact</h4>
+          <img src={images['artifact_layout.jpg']} className="artifact" alt="artifact"/>
+          
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
